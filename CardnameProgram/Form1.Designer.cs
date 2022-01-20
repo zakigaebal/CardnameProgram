1d1f9d31042c8a704b777da2feea69c2ae0e5f26
zakigaebal <86054791+zakigaebal@users.noreply.github.com>
2022-01-20 16:21:56 +0900

    초기 커밋.

---
+++ CardnameProgram/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace CardnameProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelComp = new System.Windows.Forms.Label();
+            this.textBoxComp = new System.Windows.Forms.TextBox();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.labelJob = new System.Windows.Forms.Label();
+            this.textBoxJob = new System.Windows.Forms.TextBox();
+            this.checkBoxDelShow = new System.Windows.Forms.CheckBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1300, 86);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "명함관리 프로그램";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1067, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "명함 데이터";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelName.Location = new System.Drawing.Point(87, 15);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(42, 21);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "이름";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBoxName.Location = new System.Drawing.Point(163, 15);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(150, 26);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.checkBoxDelShow);
+            this.panel2.Controls.Add(this.labelName);
+            this.panel2.Controls.Add(this.labelComp);
+            this.panel2.Controls.Add(this.labelJob);
+            this.panel2.Controls.Add(this.textBoxJob);
+            this.panel2.Controls.Add(this.labelAge);
+            this.panel2.Controls.Add(this.textBoxName);
+            this.panel2.Controls.Add(this.textBoxAge);
+            this.panel2.Controls.Add(this.textBoxComp);
+            this.panel2.Location = new System.Drawing.Point(32, 107);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(443, 202);
+            this.panel2.TabIndex = 3;
+            // 
+            // labelComp
+            // 
+            this.labelComp.AutoSize = true;
+            this.labelComp.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelComp.Location = new System.Drawing.Point(87, 56);
+            this.labelComp.Name = "labelComp";
+            this.labelComp.Size = new System.Drawing.Size(42, 21);
+            this.labelComp.TabIndex = 3;
+            this.labelComp.Text = "회사";
+            // 
+            // textBoxComp
+            // 
+            this.textBoxComp.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBoxComp.Location = new System.Drawing.Point(163, 56);
+            this.textBoxComp.Name = "textBoxComp";
+            this.textBoxComp.Size = new System.Drawing.Size(150, 26);
+            this.textBoxComp.TabIndex = 4;
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelAge.Location = new System.Drawing.Point(87, 97);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(42, 21);
+            this.labelAge.TabIndex = 5;
+            this.labelAge.Text = "나이";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBoxAge.Location = new System.Drawing.Point(163, 97);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(150, 26);
+            this.textBoxAge.TabIndex = 6;
+            // 
+            // labelJob
+            // 
+            this.labelJob.AutoSize = true;
+            this.labelJob.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelJob.Location = new System.Drawing.Point(87, 143);
+            this.labelJob.Name = "labelJob";
+            this.labelJob.Size = new System.Drawing.Size(42, 21);
+            this.labelJob.TabIndex = 7;
+            this.labelJob.Text = "직책";
+            // 
+            // textBoxJob
+            // 
+            this.textBoxJob.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBoxJob.Location = new System.Drawing.Point(163, 143);
+            this.textBoxJob.Name = "textBoxJob";
+            this.textBoxJob.Size = new System.Drawing.Size(150, 26);
+            this.textBoxJob.TabIndex = 8;
+            // 
+            // checkBoxDelShow
+            // 
+            this.checkBoxDelShow.AutoSize = true;
+            this.checkBoxDelShow.Location = new System.Drawing.Point(355, 17);
+            this.checkBoxDelShow.Name = "checkBoxDelShow";
+            this.checkBoxDelShow.Size = new System.Drawing.Size(76, 16);
+            this.checkBoxDelShow.TabIndex = 9;
+            this.checkBoxDelShow.Text = "삭제 보기";
+            this.checkBoxDelShow.UseVisualStyleBackColor = true;
+            this.checkBoxDelShow.CheckedChanged += new System.EventHandler(this.checkBoxDelShow_CheckedChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.buttonUpdate);
+            this.panel3.Controls.Add(this.buttonDelete);
+            this.panel3.Controls.Add(this.buttonSave);
+            this.panel3.Location = new System.Drawing.Point(32, 349);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(443, 118);
+            this.panel3.TabIndex = 4;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(77, 14);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(83, 85);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "저장";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(183, 14);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(83, 85);
+            this.buttonDelete.TabIndex = 1;
+            this.buttonDelete.Text = "삭제";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(286, 14);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(83, 85);
+            this.buttonUpdate.TabIndex = 2;
+            this.buttonUpdate.Text = "수정";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(510, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(760, 398);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 535);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "명함 데이터 명함관리프로그램";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelComp;
+        private System.Windows.Forms.TextBox textBoxComp;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label labelJob;
+        private System.Windows.Forms.TextBox textBoxJob;
+        private System.Windows.Forms.CheckBox checkBoxDelShow;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CardnameProgram/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CardnameProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string seqCount()
+        {
+            try
+            {
+                string MyConnection2 = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+               
+                string Query = "SELECT MAX(perSeq)+1 AS seqMax FROM dawoon.dw_persons;";
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MyConn2.Open();
+                MySqlCommand cmd = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+
+
+                    return rdr["seqMax"].ToString();
+
+                }
+
+                rdr.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return "";
+        }
+
+        private void showData()
+        {
+            try
+            {
+                string MyConnection2 = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+                //쿼리
+                string Query = "select * from dawoon.dw_persons;";//체크박스를 하면 보여주고 안보이면 안보여주는형식
+                if (checkBoxDelShow.Checked == true)
+                {
+
+                    Query = "select * from dawoon.dw_persons";
+                }
+                else
+                {
+                    Query = "select * from dawoon.dw_persons WHERE flagYN = 'Y';";
+
+                }
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                //  MyConn2 열어라 
+                //오프라인 연결을 위해서 우리는 MySqlDataAdapter class를 사용할것이다.
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+                DataTable dTable = new DataTable();
+                MyAdapter.Fill(dTable);
+                dataGridView1.DataSource = dTable;             
+                //여기서 나는 data를 보여주기 위해서 dataGridView1 객체를 dTable 객체에 할당한다.
+                                                   // MyConn2를 닫아라
+                dataGridView1.Columns[0].Visible = false;
+                dataGridView1.Columns[dataGridView1.Columns.Count - 4].Visible = false;
+                dataGridView1.Columns[dataGridView1.Columns.Count - 3].Visible = false;
+                dataGridView1.Columns[dataGridView1.Columns.Count - 2].Visible = false;
+                dataGridView1.Columns[dataGridView1.Columns.Count - 1].Visible = false;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+       
+
+        private void checkBoxDelShow_CheckedChanged(object sender, EventArgs e)
+        {
+            showData();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+            if (textBoxName.Text == "")
+            {
+                MessageBox.Show("이름을 입력해주세요");
+                textBoxName.Focus();
+                return;
+            }
+            try
+            {
+                // 이것은 나의 내가 데이터베이스 파일 주소 path에 할당했던 연결 문자열이다.
+                string MyConnection2 = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+                // 이것은 내가 윈도우폼을 통해 유저로부터 받았던 나의 insert 쿼리이다.
+                string Query = "insert into dawoon.dw_persons(perSeq,perName,perComp,perAge,perJob,flagYN,regDate,issueDate,IssueID) values('"
+                    + seqCount() + "','" + this.textBoxName.Text + "','" + this.textBoxComp.Text + "','" + this.textBoxAge.Text + "','" + this.textBoxJob.Text + "','Y',now(),now(),'CDY');";
+                
+                // 이것은 내가 객체와 패스워드를 나의 연결 스트링에 생성했었던 MySqlConnection이다.
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                //이것은 객체를 연결하고 쿼리를 핸들링 할 커맨드 클래스이다.
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader MyReader2;
+                MyConn2.Open();
+                MyReader2 = MyCommand2.ExecuteReader();      
+                // 여기서 쿼리를 실행할것이고 데이터를 데이터베이스에 저장할것이다.
+                MessageBox.Show("Save Data");
+                while (MyReader2.Read())
+                {
+
+                }
+                MyConn2.Close();
+                showData();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string seqstr = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value.ToString();
+                string MyConnection2 = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+                string Query = "update dawoon.dw_persons set flagYN='N' WHERE perSeq=" + seqstr;
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader MyReader2;
+                MyConn2.Open();
+                MyReader2 = MyCommand2.ExecuteReader();
+                MessageBox.Show("Data Deleted");
+                while (MyReader2.Read())
+                {
+
+                }
+                MyConn2.Close();
+                showData();
+                textBoxName.Text = "";
+                textBoxAge.Text = "";
+                textBoxComp.Text = "";
+                textBoxJob.Text = "";
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string seqstr = dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value.ToString();
+                
+                // 이것은 나의 내가 데이터베이스 파일 주소 path에 할당했던 연결 문자열이다.
+
+                string MyConnection2 = "datasource=127.0.0.1;port=3306;username=root;password=ekdnsel;Charset=utf8";
+                //This is my update query in which i am taking input from the user through windows forms and update the record.
+                // 이것은 레코드를 업데이트를 할 내가 윈도움폼을 통해 유저로부터 받아왔던 나의 업데이트 쿼리이다 
+                string Query = "update dawoon.dw_persons set perSeq='" + seqstr + "',perName='" + this.textBoxName.Text + "',perComp='" + this.textBoxComp.Text + "',perAge='" + this.textBoxAge.Text + "',perJob='" + this.textBoxJob.Text + "' where perSeq='" + seqstr + "';";
+                // 이것은 내가 객체와 패스워드를 나의 연결 스트링에 생성했었던 MySqlConnection이다.
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataReader MyReader2;
+                MyConn2.Open();
+                MyReader2 = MyCommand2.ExecuteReader();
+                MessageBox.Show("Data Updated");
+                while (MyReader2.Read())
+                {
+
+                }
+                MyConn2.Close();// 여기서 연결을 닫을것이다.
+                showData();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                return;
+            }
+            textBoxName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBoxComp.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            textBoxAge.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            textBoxJob.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showData();
+
+        }
+    }
+}
